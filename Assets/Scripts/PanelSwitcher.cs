80c19b731be2856f6e9f651b146627a9e67a09f9
vuong11231 <122599679+vuong11231@users.noreply.github.com>
2024-02-03 14:44:58 +0700

    anmation

---
+++ Assets/Scripts/PanelSwitcher.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// index = 0 mean open manager panel
+// index = 1 mean open ai module panel
+
+public class PanelSwitcher : MonoBehaviour
+{
+    public Animator managerPanel;
+    public Animator AIpanel;
+
+    public void SwitchPanel(int index){
+
+      //  switch(index){
+       //     case E_PanelType.Manager:
+       //     managerPanel.SetTrigger("open");
+    //        AIpanel.SetTrigger("close");
+     //       break;
+
+      //  }
+    }
+}
